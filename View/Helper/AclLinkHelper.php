8bf5b6c264582d693822b2dee81b78e15011ec83
Shahril Abdullah <shahonseven@gmail.com>
2014-08-20 11:45:17 +0800

    added acl link helper file

---
+++ View/Helper/AclLinkHelper.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * CakePHP ACL Link Helper
+ *
+ * Based on Joel Stein AclLinkHelper
+ * http://bakery.cakephp.org/articles/joel.stein/2010/06/26/acllinkhelper
+ *
+ * @author 		Shahril Abdullah - shahonseven
+ * @link 		
+ * @package 	Helper
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('FormHelper', 'View/Helper');
+App::uses('AclComponent', 'Controller/Component');
+
+class AclLinkHelper extends FormHelper {
+
+	public $userModel = 'User';
+
+	public $primaryKey = 'id';
+
+	public function __construct(View $View, $settings = array()) {
+		parent::__construct($View, $settings);
+
+		if (is_array($settings) && isset($settings['userModel'])) {
+			$this->userModel = $settings['userModel'];
+		}
+
+		if (is_array($settings) && isset($settings['primaryKey'])) {
+			$this->primaryKey = $settings['primaryKey'];
+		}
+	}
+
+	protected function _aclCheck($url) {
+		$plugin = '';
+		if (isset($url['plugin'])) {
+			$plugin = Inflector::camelize($url['plugin']) . '/';
+		}
+
+		$controller = '';
+		if (isset($url['controller'])) {
+			$controller = Inflector::camelize($url['controller']) . '/';
+		}
+
+		$action = 'index';
+		if (isset($url['action'])) {
+			$action = $url['action'];
+		}
+
+		$collection = new ComponentCollection();
+        $acl = new AclComponent($collection);
+        $aro = array(
+        	$this->userModel => array(
+        		$this->primaryKey => AuthComponent::user($this->primaryKey)
+        	)
+        );
+        $aco = $plugin.$controller.$action;
+        return $acl->check($aro, $aco);
+	}
+
+	public function link($title, $url = null, $options = array(), $confirmMessage = null) { 
+		if ($this->_aclCheck($url)) {
+			return $this->Html->link($title, $url, $options, $confirmMessage);
+		}
+		return '';
+	}
+
+	public function postLink($title, $url = null, $options = array(), $confirmMessage = false) {
+		if ($this->_aclCheck($url)) {
+			return parent::postLink($title, $url, $options, $confirmMessage);
+		}
+		return '';
+	}
+	
+}
